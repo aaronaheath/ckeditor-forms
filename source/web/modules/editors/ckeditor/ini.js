7804ae079889b4273997b28608d8ad4d1ec7ba75
mrestine <matt_restine@s5a.com>
2014-12-09 10:31:44 -0500

    CKEDITOR_BASEPATH is no longer relative

--- source/web/modules/editors/ckeditor/ini.js
+++ source/web/modules/editors/ckeditor/ini.js
@@ -1,4 +1,5 @@
 
 // Alfresco 4.x path rewrite :
-var CKEDITOR_BASEPATH = '../../../res/modules/editors/ckeditor/';
+//var CKEDITOR_BASEPATH = '../../../res/modules/editors/ckeditor/';
+var CKEDITOR_BASEPATH = '/share/res/modules/editors/ckeditor/';
 
